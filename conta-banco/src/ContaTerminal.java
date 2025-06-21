6d7f3f54eed022c7603672205beef3b43edc7445
Dhonata Chaves Santos <dhonatachaves7@gmail.com>
2025-06-21 20:04:07 -0300

    feat: implementa captura de dados bancários via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!.");
+        
+        Scanner entrada = new Scanner(System.in);
+
+        // Declaração de variáveis para o projeto
+        String agency;
+        String customerName;
+        int accountNumber;
+        double bankBalance;
+
+        // Captura dados bancários do usuário
+        System.out.println("Nome do cliente: ");
+        customerName = entrada.nextLine();
+        System.out.println("número da conta: ");
+        accountNumber = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Número da agência: ");
+        agency = entrada.nextLine();
+        System.out.println("Saldo bancário: R$");
+        bankBalance = entrada.nextDouble();
     }
 }
