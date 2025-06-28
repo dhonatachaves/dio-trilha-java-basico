e5d89692c059add43b5821638fded23989dbd48d
Dhonata Chaves Santos <dhonatachaves7@gmail.com>
2025-06-28 20:45:31 -0300

    feat: adiciona exceção personalizada e validação de parâmetros

--- desafios/controle-fluxo/src/contador.java
+++ desafios/controle-fluxo/src/contador.java
@@ -1,15 +1,37 @@
 import java.util.Scanner;
 
-public class contador {
+public class Contador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o primeiro parâmetro: ");
         int valorInicial = scanner.nextInt();
-        System.out.println("Digite o segundo paramêtro: ");
+        System.out.println("Digite o segundo parâmetro: ");
         int valorFinal = scanner.nextInt();
 
-        scanner.close();
+        
+        try {
+            contar(valorInicial, valorFinal);  
+
+        }catch(ParametrosInvalidosException erroParametro) {   
+           System.out.println(erroParametro.getMessage());
+
+        }finally {
+            scanner.close();
+        }
+        
+    }
+
+    static void contar(int valorInicial, int valorFinal) throws ParametrosInvalidosException {
+
+        if(valorInicial > valorFinal) throw new ParametrosInvalidosException("Segundo parâmetro deve ser maior que o primeiro.");
+        int contagemFor = valorFinal - valorInicial;
+
+        for(int i = 1; i <= contagemFor; i++) {
+            System.out.printf("Imprimindo o número: %d\n", i);
+        }
 
     }
+
+    
 }
